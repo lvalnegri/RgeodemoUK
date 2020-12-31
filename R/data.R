9fd920fab498dd8e505a11a6038b2fcef785f455
Luca Valnegri <l.valnegri@datamaps.co.uk>
2020-12-31 14:55:11 +0000

    first commit

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' @importFrom data.table data.table
+NULL
+
+#' Population
+#'
+#' A dataset containing the UK population for the last available year by Output Area
+#'
+#' @format A data.table with 4 columns:
+#' \describe{
+#'   \item{\code{OA}}{output area ONS code}
+#'   \item{\code{count}}{the value for the population}
+#' }
+#'
+#' For further details, see \url{http://}
+#'
+'population'
+
