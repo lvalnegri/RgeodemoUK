30535ecaeb2964c3c5f4bf891ec98a2b1f8db74d
datamaps <l.valnegri@datamaps.co.uk>
2022-07-30 01:27:11 +0100

    commit soon after change in package name

---
+++ R/RgeodemoUK.R
@@ -0,0 +1,8 @@
+#' RgeodemoUK
+#' 
+#' This package consists of datasets designed to help Data Sourcing, Cleaning and Wrangling of 
+#' British geographical data, plus a bunch of other visualization and geographical stuff.
+#'
+#' @docType package
+#' @name RgeodemoUK
+NULL
